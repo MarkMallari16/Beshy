91b6e92c4121999bde03f716b6c146494c349e6b
MarkMallari16 <104248911+MarkMallari16@users.noreply.github.com>
2023-08-11 23:14:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
-const convertBeshy = document.getElementById("convert").addEventListener("click", () =>{
+const convertBeshy = document.getElementById("convert").addEventListener("click", () => {
     let text = document.getElementById("text").value;
     let inputResult = document.getElementById("inputResult");
     let beshyText;
-    const beshyEmoji  = "🤸";
-    
-    if (text.match(/\s/g)){
+    const beshyEmoji = "🤸";
+
+    if (text.match(/\s/g)) {
         beshyText = text.replace(/\s/g, beshyEmoji);
         inputResult.value = beshyText;
-    }else{
+    } else {
         inputResult.value = "You must enter space first!";
     }
 });
 
-const clear = document.getElementById("clear").addEventListener("click", () =>{
+const clear = document.getElementById("clear").addEventListener("click", () => {
     let text = document.getElementById("text");
     let inputResult = document.getElementById("inputResult");
-    
+
     text.value = "";
     inputResult.value = "";
-   
+
 });
 
 const copy = document.getElementById("copyClipboard").addEventListener("click", () => {
     let inputResult = document.getElementById("inputResult");
     let textCopy = inputResult.value;
-   
-    if (navigator.clipboard && textCopy){
+    const checkIcon = document.querySelector('.check');
+    if (navigator.clipboard && textCopy) {
         navigator.clipboard.writeText(textCopy)
-            .then(() =>{
+            .then(() => {
+
                 alert("Text copied to clipboard");
-            }).catch((error) =>{
-                console.error("Error to copying text to clipboard: " , error);
+                checkIcon.style.display = "block";
+                setTimeout(function () {
+                    checkIcon.style.display = "none";
+                }, 3000);
+
+            }).catch((error) => {
+                console.error("Error to copying text to clipboard: ", error);
             });
     }
+
 });
